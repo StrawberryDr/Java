08cdd4e9c04cde8abb541252197557cd79b6e554
StrawberryDr <1471612073@qq.com>
2019-10-31 13:14:55 +0800

    20191031

--- 反转单链表.java
+++ 反转单链表.java
@@ -14,17 +14,17 @@ class ListNode{
 public class SingleList {
     public ListNode head;
 
-    public SingleList(){
+    public SingleList() {
     }
 
     //头插法
     //1、判断是不是第一次插入；
     //2、进行插入操作
-    public void addFirst(int data){
+    public void addFirst(int data) {
         ListNode node = new ListNode(data);
-        if(this.head == null){
+        if (this.head == null) {
             this.head = node;
-        }else{
+        } else {
             node.next = this.head;
             this.head = node;
         }
@@ -33,13 +33,13 @@ public class SingleList {
     //尾插法
     //1、如果是第一次插入即head == null;
     //2、不是第一次插入：cur从头开始遍历，直到cur.next == null时，将node赋给cur.next
-    public void addLast(int data){
+    public void addLast(int data) {
         ListNode node = new ListNode(data);
         ListNode cur = this.head;
-        if(this.head == null){
+        if (this.head == null) {
             this.head = node;
-        }else {
-            while (cur.next != null){
+        } else {
+            while (cur.next != null) {
                 cur = cur.next;
             }
             cur.next = node;
@@ -47,32 +47,33 @@ public class SingleList {
     }
 
     //任意位置插入，第一个数据节点为0号下标
-    public void addIndex(int index,int data){
+    public void addIndex(int index, int data) {
         ListNode node = new ListNode(data);
-        if(index < 0 || index > size()){
+        if (index < 0 || index > size()) {
             System.out.println("输入index不合法");
-        }else if(index == 0){
+        } else if (index == 0) {
             addFirst(data);
-        }else{
+        } else {
             ListNode cur = searchIndex(index);
             node.next = cur.next;
             cur.next = node;
         }
     }
+
     //找到index标号的前一个
-    private ListNode searchIndex(int index){
+    private ListNode searchIndex(int index) {
         ListNode cur = this.head;
-        for(int i = 0;i < index-1;i++){
+        for (int i = 0; i < index - 1; i++) {
             cur = cur.next;
         }
         return cur;
     }
 
     //查找是否包含关键字key
-    public boolean contains(int key){
+    public boolean contains(int key) {
         ListNode cur = this.head;
-        while(cur != null){
-            if(cur.data == key){
+        while (cur != null) {
+            if (cur.data == key) {
                 return true;
             }
             cur = cur.next;
@@ -81,19 +82,19 @@ public class SingleList {
     }
 
     //删除第一次出现的关键字key
-    public void remove(int key){
+    public void remove(int key) {
         ListNode cur = this.head;
         ListNode temp = this.head.next;
-        if(this.head == null) {
+        if (this.head == null) {
             System.out.println("单链表为空");
             return;
         }
-        if(this.head.data == key) {
+        if (this.head.data == key) {
             this.head = this.head.next;
-        }else if(temp.data == key){
+        } else if (temp.data == key) {
             cur.next = temp.next;
             temp = cur.next;
-        }else{
+        } else {
             cur = temp;
             temp = temp.next;
         }
@@ -101,20 +102,20 @@ public class SingleList {
 
 
     //删除所有值为key的节点
-    public void removeAll(int key){
+    public void removeAll(int key) {
         ListNode cur = this.head;
         ListNode temp = this.head.next;
-        if(this.head == null) {
+        if (this.head == null) {
             System.out.println("单链表为空");
             return;
         }
-        while (temp != null){
-            if(this.head.data == key){
+        while (temp != null) {
+            if (this.head.data == key) {
                 this.head = this.head.next;
-            }else if(temp.data == key){
+            } else if (temp.data == key) {
                 cur.next = temp.next;
                 temp = cur.next;
-            }else{
+            } else {
                 cur = temp;
                 temp = temp.next;
             }
@@ -122,43 +123,43 @@ public class SingleList {
     }
 
     //单链表长度
-    public int size(){
+    public int size() {
         ListNode cur = this.head;
         int size = 0;
-        while(cur.next != null){
-            size ++;
+        while (cur.next != null) {
+            size++;
             cur = cur.next;
         }
         return size;
     }
 
     //打印单链表
-    public void display(){
+    public void display() {
         ListNode cur = this.head;
-        while(cur != null){
+        while (cur != null) {
             System.out.println(cur.data + " ");
             cur = cur.next;
         }
     }
 
     //清空单链表
-    public void clear(){
+    public void clear() {
         ListNode cur = this.head.next;
-        while (this.head != null){
+        while (this.head != null) {
             this.head = null;
             this.head = cur;
         }
     }
 
-//反转
-    public void reverseList(){
+    //反转
+    public void reverseList() {
         ListNode prev = null;
         ListNode newHead = null;
         ListNode cur = this.head;
 
-        while(cur != null){
+        while (cur != null) {
             ListNode curNext = cur.next;
-            if(curNext == null){
+            if (curNext == null) {
                 newHead = cur;
             }
             cur.next = prev;
@@ -168,7 +169,7 @@ public class SingleList {
         this.head = newHead;
     }
 
-//链表的中间节点:一快一慢指针
+    //链表的中间节点:一快一慢指针
     public ListNode middleNode() {
         ListNode fast = this.head;
         ListNode slow = this.head;
@@ -178,17 +179,6 @@ public class SingleList {
         }
         return slow;
     }
-
-//输出倒数第K个节点
-    public ListNode findKthToTail(int k){
-        if(k <= 0 || head == null){
-            return null;
-        }
-        ListNode fast = this.head;
-        ListNode slow = this.head;
-
-
-
-    }
-
 }
+
+
